375e603a381035b57e74285b81ab1f3c1828ad8a
Roman Zimmermann <roman@more-onion.com>
2019-03-13 19:54:08 +0100

    New selector for opt-in channels.

---
+++ src/Exporter/WebformFormatted/OptInChannelSelector.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Drupal\campaignion_csv\Exporter\WebformFormatted;
+
+use Drupal\campaignion_opt_in\Values;
+use Drupal\little_helpers\Webform\Submission;
+
+/**
+ * Select webform opt-in submission values based on their channel.
+ */
+class OptInChannelSelector implements SelectorInterface {
+
+  protected $channel;
+
+  /**
+   * Create a new instance from an info-array.
+   *
+   * @param array $info
+   *   The info array. The following keys are in use:
+   *   - channel: The selected channel.
+   */
+  public static function fromInfo(array $info) {
+    $info += ['channel' => 'email'];
+    return new static($info['channel']);
+  }
+
+  /**
+   * Create a new instance.
+   *
+   * @param string $channel
+   *   The opt-in channel to select.
+   */
+  public function __construct($channel) {
+    $this->channel = $channel;
+  }
+
+  /**
+   * Get the value for a specific submission.
+   *
+   * @param \Drupal\little_helpers\Webform\Submission $submission
+   *   The webform submission.
+   *
+   * @return mixed
+   *   The first non-NULL value for this submission or NULL if none was found.
+   */
+  public function value(Submission $submission) {
+    foreach ($submission->webform->componentsByType('opt_in') as $cid => $component) {
+      $value = $submission->valueByCid($cid);
+      if (!is_null($value) && $component['extra']['channel'] == $this->channel) {
+        return Values::removePrefix($value);
+      }
+    }
+    return NULL;
+  }
+
+}
